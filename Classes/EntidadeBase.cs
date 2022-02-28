04c7c82d4f6c066f6c47d50fdf742e42a812f61b
joaosebbe <joaovitor.sebbe@yahoo.com>
2022-02-28 02:43:42 -0300

    Adicionar arquivos de projeto.

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace ProjectDioSeries
+{
+    public abstract class EntidadeBase
+    {
+        public int Id{ get; protected set; }
+    }
+}
---
+++ Interface/iRepositorio.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace ProjectDioSeries.Interface
+{
+    public interface iRepositorio<T>
+    {
+         List<T> Lista();
+         T RetornaPorId(int id);
+         void Insere(T entidade);
+         void Exclui(int id);
+         void Atualiza(int id, T entidade);
+         int ProximoId();
+
+    }
+}
